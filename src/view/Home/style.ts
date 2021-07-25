a4254b70cf63f51937fde4ec883e39b367fb2d7d
alexmmalta <87395747+alexmmalta@users.noreply.github.com>
2021-07-25 17:25:32 -0300

    melhorias

--- src/view/Home/style.ts
+++ src/view/Home/style.ts
@@ -63,7 +63,7 @@ export const Container = styled.div`
             text-align: center;
             height: 300px;
             background: #fff;
-            border-radius: 5px;
+/*             border-radius: 5px; */
             padding: 12px;
             box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
             transition: all 0.3s cubic-bezier(.25,.8,.25,1);
