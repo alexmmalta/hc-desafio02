2cea61f22a68b74b530b9a5240e213be6ef648af
alexmmalta <87395747+alexmmalta@users.noreply.github.com>
2021-07-25 20:23:03 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://my-json-server.typicode.com/alexmmalta/fakeapi/produtos',
+    baseURL: 'https://my-json-server.typicode.com/alexmmalta/hc-desafio02/produtos',
     timeout: 1000
 });
 
