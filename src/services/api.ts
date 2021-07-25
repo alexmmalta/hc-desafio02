70992bd31851d5e54ff9aa1c7ada4603dfd295e3
alexmmalta <87395747+alexmmalta@users.noreply.github.com>
2021-07-25 18:22:11 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8888/produtos'
+    baseURL: 'http://localhost:8888/produtos',
+    timeout: 1000
 });
 
 export default api;
